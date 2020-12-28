35ca1102b4a948144ad4ee3bed8b9ba653913184
Dmitry Palchynski <palchmail@gmail.com>
2020-12-28 16:41:37 +0300

    init

---
+++ utils/src/test/java/by/mjc/utils/StringUtilsTest.java
@@ -0,0 +1,45 @@
+package by.mjc.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringUtilsTest {
+
+    @ParameterizedTest
+    @MethodSource("dataForIsPositiveNumberCorrectTest")
+    void isPositiveNumberCorrect(String str) {
+        boolean actual = StringUtils.isPositiveNumber(str);
+        assertTrue(actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataForIsPositiveNumberIncorrectTest")
+    void isPositiveNumberIncorrect(String str) {
+        boolean actual = StringUtils.isPositiveNumber(str);
+        assertFalse(actual);
+    }
+
+    private static Stream<Arguments> dataForIsPositiveNumberCorrectTest() {
+        return Stream.of(
+                Arguments.of("123"),
+                Arguments.of("12.2"),
+                Arguments.of("17.3"),
+                Arguments.of("17"));
+    }
+
+    private static Stream<Arguments> dataForIsPositiveNumberIncorrectTest() {
+        return Stream.of(
+                Arguments.of("  123"),
+                Arguments.of("12.2 3"),
+                Arguments.of("-17,3"),
+                Arguments.of("-17"),
+                Arguments.of("-17.3"),
+                Arguments.of("17a"));
+    }
+}
